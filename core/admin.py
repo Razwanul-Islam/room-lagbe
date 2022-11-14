68c3a70df8dfd1cd95524493157336cead344928
Md. Razwanul Islam <razwanrakib0@gmail.com>
2022-11-14 23:50:48 +0600

    Jazzmin dashboard style

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,6 @@ from .models import Hotel,Room
 
 class RoomInHotel(admin.TabularInline):
     model = Room
-
 class HotelAdmin(admin.ModelAdmin):
     model = Hotel
     list_display = ["id","name","location"]
--- room_lagbe/urls.py
+++ room_lagbe/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path('accounts/', include('allauth.urls')),
      path('tinymce/', include('tinymce.urls')),
+     path("i18n/", include("django.conf.urls.i18n")),
      path('',include('core.urls')),
 ]
 # media and static files urls
